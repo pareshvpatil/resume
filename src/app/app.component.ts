e78e660162cb8c6c0e91888ce69d7fa8e6725e6d
Paresh V Patil <paresh.zxc111@gmail.com>
2024-05-18 09:43:16 +0530

    Modified: tenure logic to show months and years

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration"
+import {from} from "rxjs";
 
 dayjs.extend(duration)
 
@@ -13,9 +14,15 @@ export class AppComponent {
   title = 'resume';
   year = new Date().getUTCFullYear();
 
-  tenure(month: number, year: number, startMonth?: number, startYear?: number) {
-    const tenureYears = dayjs().diff(dayjs().month(month).year(year).toDate(), 'years');
-    const tenureMonths = dayjs().diff(dayjs().subtract(1, 'year').month(month), 'months');
+  tenure(fromMonth: number, fromYear: number, toMonth?: number, toYear?: number) {
+    let fromDate = dayjs().month(fromMonth).year(fromYear);
+    let toDate = dayjs();
+    if (toMonth && toYear) {
+      toDate = toDate.month(toMonth).year(toYear);
+    }
+    const tenureYears = toDate.diff(fromDate, 'years');
+    let tenureMonths = Math.abs(fromDate.month() - toDate.month());
+    tenureMonths = fromDate.month() > toDate.month() ? (12 - tenureMonths) : tenureMonths;
 
     const yearText = `${tenureYears > 0 ? tenureYears + ' Years' : ''}`
     const monthText = `${tenureMonths > 0 ? tenureMonths + ' Months' : ''}`
