3b6d8fef985186597b970b0d197969d5b3048892
Paresh V Patil <paresh.zxc111@gmail.com>
2024-05-18 08:29:08 +0530

    Modified: skill percentages to better values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration"
+
+dayjs.extend(duration)
 
 @Component({
   selector: 'app-root',
@@ -8,4 +12,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'resume';
   year = new Date().getUTCFullYear();
+
+  tenure(month: number, year: number, startMonth?: number, startYear?: number) {
+    const tenureYears = dayjs().diff(dayjs().month(month).year(year).toDate(), 'years');
+    const tenureMonths = dayjs().diff(dayjs().subtract(1, 'year').month(month), 'months');
+
+    const yearText = `${tenureYears > 0 ? tenureYears + ' Years' : ''}`
+    const monthText = `${tenureMonths > 0 ? tenureMonths + ' Months' : ''}`
+
+    return `${yearText.length ? yearText + ', ' + monthText : monthText}`
+  }
 }
